9a7513fd298c0d18d56706cdf015c26897da770c
vinicios <vinicompif@gmail.com>
2021-02-23 11:24:57 -0300

    controller mde categories aprimorado

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -51,4 +51,39 @@ router.post('/categories/delete', (req, res)=>{
     }
 })
 
+
+router.get('/admin/categories/edit/:id', (req, res)=>{
+
+    const id = req.params.id;
+
+    if(!isNaN(id)){
+        Category.findByPk(id).then((category)=>{
+            if(category != undefined){
+                res.render('./admin/categories/edit', {category: category});
+            }else{
+                res.redirect('/admin/categories');
+            }
+        }).catch((error)=>{
+            res.redirect('/admin/categories');
+        })
+    }else{
+        res.redirect('/admin/categories');
+    }
+})
+
+router.post('/categories/update', (req, res)=>{
+    const id = req.body.id;
+    const title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)}, {
+        where:{
+            id:id,
+        }
+    }).then(()=>{
+        res.redirect('/admin/categories');
+    })
+
+})
+
 module.exports = router;
+
