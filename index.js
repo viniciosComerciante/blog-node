e32753a66dd7b1fe17de7293eec59d43a015fc3c
vinicios <vinicompif@gmail.com>
2021-02-24 14:06:47 -0300

    pull da mastter

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ app.use('/', articlesController);
 
 
 app.get('/', (req, res)=>{
-    Article.findAll().then((articles)=>{
+    Article.findAll({
+        order:[
+            ['id', 'DESC']
+        ]
+    }).then((articles)=>{
         res.render('index', {articles: articles});
     })
 })
