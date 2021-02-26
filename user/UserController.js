d22c9d42d16e692d00928840792fe103d8cf3e15
vinicios <vinicompif@gmail.com>
2021-02-25 22:55:38 -0300

    adicionado sistema de usuario e validaçao basica na criacao de usuarios

--- user/UserController.js
+++ user/UserController.js
@@ -1,24 +1,64 @@
 const express = require('express');
 const router  = express.Router();
 const User = require('./User');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res)=>{
 
-    res.send("listagem de usuarios");
+    User.findAll().then((users)=>{
+            res.render('./admin/users/index', {users:users});
+    })
+
 
 });
 
 
 router.get('/admin/users/create', (req, res)=>{
-    res.render('./admin/users/create');
+    res.render('./admin/users/create', {erro:''});
 })
 
 router.post("/users/create", (req, res)=>{
 
     const email = req.body.email;
     const password = req.body.password;
+    let erro = '';
+    if(email ==''){
+        erro = "email precisa ser preenchido";
+    }
 
-    res.json({email, password})
+    User.findOne({
+        where:{
+            email: email,
+        }
+    }).then((user)=>{
+        if(user == undefined){
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+
+            User.create({
+                email: email,
+                password: hash,
+
+            }).then(()=>{
+                res.redirect('/admin/users');
+                
+            }).catch((err)=>{
+                res.send('não foi possivel criar usuario');
+            })
+        }else{
+            if(erro ==''){
+                erro = "usuario já cadastrado";
+                res.render('./admin/users/create', {erro: erro})
+            }else{
+                res.render('./admin/users/create', {erro: erro})
+            }
+            
+        }
+    })
+
+
+    
+
 })
 
 
